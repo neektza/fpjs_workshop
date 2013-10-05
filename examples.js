8026d1a8eaf99d521bcda35eecbe2c612ca349c5
Veljko Dragsic <veljko@kset.org>
2013-10-05 14:52:59 +0200

    Examples for high order functions.

--- examples.js
+++ examples.js
@@ -1,8 +1,8 @@
 /*
-* --------------------------------
-* Fja osnovna jedinica apstrakcije
-* --------------------------------
-*/
+ * --------------------------------
+ * Fja osnovna jedinica apstrakcije
+ * --------------------------------
+ */
 
 // Ex 1 - problem
 function parseAge(age) {
@@ -51,7 +51,68 @@ function parseAge(age) {
 }
 
 /*
-* -------------------------
-* First class FNs, Closures
-* -------------------------
-*/
+ * -------------------------
+ * First class FNs, Closures
+ * -------------------------
+ */
+
+
+
+/*
+ * -------------------------
+ * High Order Functions
+ * -------------------------
+ */
+
+
+// functions that take other functions
+
+var plucker = function( attr ) {
+	return function(obj) {
+		return (obj && obj[attr]);
+	};
+}
+
+var finder = function( valueFun, bestFun, coll ) {
+	return _.reduce(coll, function( best, current ) {
+		var bestValue = valueFun(best);
+		var currentValue = valueFun(current);		
+		return (bestValue === bestFun(bestValue, currentValue)) ? best : current;
+	});
+}
+
+var people = [{name: "Ivica", age: 54}, {name: "Marica", age: 18}];
+
+//run: finder( plucker('age'), Math.max, people);
+
+
+// functions that return other functions
+
+var always = function( value ) {
+	return function() {
+		return value;
+	}
+}
+
+var checker = function( /* validators */ ) {
+	var validators = _.toArray( arguments );
+	return function( obj ) {
+		return _.reduce( validators, function( errs, check ) {
+			if ( check(obj) )
+				return errs
+			else
+				return _.chain( errs ).push( check.message ).value();
+		}, []);
+	}
+}
+
+function validator(message, fun) {
+	var f = function(/* args */) {
+		return fun.apply(fun, arguments);
+	};
+	f['message'] = message;
+	return f;
+}
+
+//run: var gonnaFail = checker(validator("ZOMG!", always(false)));
+//run: gonnaFail(100);
